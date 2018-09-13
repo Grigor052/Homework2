79a3db8d48716f2d76342a829b86fcecc7472591
Margarita Tovmasyan <margaritatovmasyan@Margaritas-MacBook-Pro.local>
2018-09-13 20:42:47 +0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+const a = 5;
+const b = 64;
+const sum = a + b;
+const product = a*b;
+
+
+
+console.log("Grigor's club");
+
+const name = "Grigor";
+if (name=="Grigor") {
+	console.log("Hi Grigor, you can be member of our group");
+} else{
+	console.log("You cannot be member of our group, becouse your name is not Grigor");
+}
